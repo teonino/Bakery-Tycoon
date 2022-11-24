93b97009e2eefac13978f7bd8621aed72c7aaa82
ArnaudVdps <107640450+ArnaudVdps@users.noreply.github.com>
2022-11-24 17:30:05 +0100

    Camera Begin + shader

---
+++ Assets/Scripts/Player/CameraSwitch.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] private GameObject CameraMainRoomPosition;
+    [SerializeField] private GameObject CameraStorageRoomPosition;
+    [SerializeField] private Collider TriggerMainRoom;
+    private float lerpRange = 0.0f;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            cam.transform.position = new Vector3.Lerp(CameraMainRoomPosition.transform.position, CameraStorageRoomPosition.transform.position, lerpRange);
+        }
+        print("Storage");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("MainRoom");
+        
+    }
+
+
+
+}
