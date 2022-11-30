29fee357bb5c71ba3eaa9da26d0a911dd798a1ac
ArnaudVdps <107640450+ArnaudVdps@users.noreply.github.com>
2022-11-30 17:52:13 +0100

    begin camera switch

--- Assets/Scripts/Player/CameraSwitch.cs
+++ Assets/Scripts/Player/CameraSwitch.cs
@@ -5,23 +5,43 @@ using UnityEngine.InputSystem;
 
 public class CameraSwitch : MonoBehaviour
 {
-    [SerializeField] private GameObject CameraObject;
-    [SerializeField] private float rotateSpeed = 300f;
-    public GameManager gameManager;
+    private float playerLocalisation = 0;
+    [SerializeField] private GameObject MainRoomSocket;
+    [SerializeField] private GameObject StorageCamSocket;
+    private float LerpTime = 0.5f;
 
     private void Update()
     {
-        Quaternion rotateDir = new Quaternion();
-        rotateDir *= Quaternion.AngleAxis(gameManager.GetPlayerController().playerInput.Player.Camera.ReadValue<Vector2>().x * Time.deltaTime * 10, Vector3.up);
+        if(playerLocalisation == 0)
+        {
+            print("Default");
+            MainRoomSocket.transform.position = MainRoomSocket.transform.position;
+        }
+        else if (playerLocalisation == 1)
+        {
+            print("MainRoom");
+            MainRoomSocket.transform.position = Vector3.Slerp(StorageCamSocket.transform.position, MainRoomSocket.transform.position, LerpTime * Time.deltaTime);
 
 
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    rotateDir -= 1f;
-        //}
+        }
+        else if (playerLocalisation == 2)
+        {
+            print("Storage");
+        }
+    }
 
-        transform.rotation = rotateDir;
+    private void OnTriggerEnter(Collider other)
+    {
+        playerLocalisation = 1;
+        MainRoomSocket.transform.position = Vector3.Slerp(MainRoomSocket.transform.position, StorageCamSocket.transform.position, LerpTime * Time.deltaTime);
 
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerLocalisation = 2;
+
+    }
+
 
 }
