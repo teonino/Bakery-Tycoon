292efc153e829b113e574cc98aac248168510992
Arnaud BM <arnaudbm26@gmail.com>
2022-12-02 14:18:37 +0100

    Fix fading walls

--- Assets/Scripts/Player/WallFade.cs
+++ Assets/Scripts/Player/WallFade.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallFade : MonoBehaviour
-{
+public class WallFade : MonoBehaviour {
 
     [SerializeField] private GameObject wallToDispawn;
     [SerializeField][Range(5f, 15f)] float time;
@@ -12,59 +11,48 @@ public class WallFade : MonoBehaviour
     private List<Coroutine> invisibleCoroutines;
     private List<Coroutine> visibleCoroutines;
 
-    private void Start()
-    {
+    private void Start() {
         lerpTime = time * Time.deltaTime;
         invisibleCoroutines = new List<Coroutine>();
         visibleCoroutines = new List<Coroutine>();
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (thisRoomIsActive)
-        {
-            if (other.gameObject.name == "Main Camera")
-            {
+    private void OnTriggerEnter(Collider other) {
+        if (thisRoomIsActive) {
+            if (other.gameObject.name == "Main Camera") {
                 if (visibleCoroutines.Count > 0) {
                     foreach (Coroutine coroutine in visibleCoroutines)
                         StopCoroutine(coroutine);
                     visibleCoroutines.Clear();
                 }
 
-                for (int i = 0; i < wallToDispawn.transform.childCount; i++)
-                {
+                for (int i = 0; i < wallToDispawn.transform.childCount; i++) {
                     invisibleCoroutines.Add(StartCoroutine(ChangeColor(wallToDispawn.transform.GetChild(i), 0)));
                 }
             }
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (thisRoomIsActive)
-        {
-            if (other.gameObject.name == "Main Camera")
-            {
+    private void OnTriggerExit(Collider other) {
+        if (thisRoomIsActive) {
+            if (other.gameObject.name == "Main Camera") {
                 if (invisibleCoroutines.Count > 0) {
                     foreach (Coroutine coroutine in invisibleCoroutines)
                         StopCoroutine(coroutine);
                     invisibleCoroutines.Clear();
                 }
 
-                for (int i = 0; i < wallToDispawn.transform.childCount; i++)
-                {
+                for (int i = 0; i < wallToDispawn.transform.childCount; i++) {
                     visibleCoroutines.Add(StartCoroutine(ChangeColor(wallToDispawn.transform.GetChild(i), 1)));
                 }
             }
         }
     }
 
-    private IEnumerator ChangeColor(Transform go, float opacity)
-    {
-        while (go.GetComponent<Renderer>().material.color.a != opacity)
-        {
-            Color c = go.GetComponent<Renderer>().material.color;
-            go.GetComponent<Renderer>().material.color = Color.Lerp(c, new Color(c.r,c.g,c.b, opacity), lerpTime);
+    private IEnumerator ChangeColor(Transform go, float opacity) {
+        while (go.GetComponent<Renderer>().material.GetFloat("_IntForce") != opacity) {
+            float a = go.GetComponent<Renderer>().material.GetFloat("_IntForce");
+            go.GetComponent<Renderer>().material.SetFloat("_IntForce", Mathf.Lerp(a, opacity, lerpTime));
             yield return new WaitForEndOfFrame();
         }
         yield return null;
