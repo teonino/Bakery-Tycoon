8b42222741010296af30affe540fa6bcd345a068
ArnaudVdps <107640450+ArnaudVdps@users.noreply.github.com>
2022-12-06 12:23:33 +0100

    begin tuto

---
+++ Assets/Scripts/UI/Tutorial/tutoManagement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class tutoManagement : MonoBehaviour
+{
+    [SerializeField] private GameObject tutoPanel;
+    [SerializeField] private TextMeshProUGUI dialogueTest;
+    [SerializeField] private GameObject questPanel;
+
+    private void Start()
+    {
+
+    }
+
+}
