6de61c47f917c5ce1fbda5852f43e79161d55124
ArnaudVdps <107640450+ArnaudVdps@users.noreply.github.com>
2023-01-04 11:46:53 +0100

    MainMenu Clean

--- Assets/Scripts/UI/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -10,50 +10,15 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private List<GameObject> Screen = new List<GameObject>();
     [SerializeField] private List<GameObject> Button = new List<GameObject>();
     [SerializeField] private PlayerInput playerInput;
-    [SerializeField] private bool pressAnyButton = false;
 
-    //void Start()
-    //{
-    //    playerInput = new PlayerInput();
-    //    playerInput.UI.Enable();
-    //    playerInput.UI.AnyKeyPressed.performed += Continue;
-    //    playerInput.UI.Click.performed += Continue;
-    //    ScreenAnimator[0].SetTrigger("TriggerFade");
-    //    StartCoroutine(DisplayTitle());
-    //    StartCoroutine(WaitingTimeToDisableBlackscreen());
-    //    StartCoroutine(DisplayLogo());
-    //}
 
-    //public void Continue(InputAction.CallbackContext context)
-    //{
-    //    if (context.performed)
-    //    {
-    //        StartCoroutine(AnimationLogo());
-    //    }
-    //}
+
 
     public void displayMainMenuTools()
     {
         StartCoroutine(DisplayButton());
     }
 
-    public IEnumerator AnimationLogo()
-    {
-        if (pressAnyButton)
-        {
-            //ScreenAnimator[1].SetTrigger("TriggerMovement");
-            //ScreenAnimator[2].SetTrigger("TriggerHide");
-            yield return new WaitForSeconds(3);
-            StartCoroutine(DisplayButton());
-            pressAnyButton = false;
-        }
-        else
-        {
-            print("Not this time");
-        }
-
-
-    }
 
     public IEnumerator DisplayButton()
     {
@@ -61,38 +26,10 @@ public class MainMenuManager : MonoBehaviour
         Button[1].SetActive(true);
         Button[2].SetActive(true);
         yield return new WaitForSeconds(0.3f);
-        //ScreenAnimator[3].SetTrigger("Spawn");
-        //ScreenAnimator[4].SetTrigger("Spawn");
-        //ScreenAnimator[5].SetTrigger("Spawn");
+
 
     }
 
-    private IEnumerator DisplayLogo()
-    {
-        yield return new WaitForSeconds(1);
-        Screen[1].SetActive(true);
-        //ScreenAnimator[1].SetTrigger("TriggerFade");
-        yield return new WaitForSeconds(0.5f);
-        pressAnyButton = true;
-        Screen[2].SetActive(true);
-        //ScreenAnimator[2].SetTrigger("TriggerDisplay");
-    }
-
-    private IEnumerator DisplayTitle()
-    {
-        yield return new WaitForSeconds(1);
-    }
-
-    private IEnumerator WaitingTimeToDisableBlackscreen()
-    {
-        yield return new WaitForSeconds(3f);
-        DisableBlackScreen();
-    }
-
-    private void DisableBlackScreen()
-    {
-        Screen[0].SetActive(false);
-    }
 
     public void SetQuality(int qualityIndex)
     {
