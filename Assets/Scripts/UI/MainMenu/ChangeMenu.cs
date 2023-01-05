d22eefc891e20c28277f358b63b70ad0aaed51c2
ArnaudVdps <107640450+ArnaudVdps@users.noreply.github.com>
2023-01-05 14:48:45 +0100

    MainMenu is clean & Cutlery Import

--- Assets/Scripts/UI/MainMenu/ChangeMenu.cs
+++ Assets/Scripts/UI/MainMenu/ChangeMenu.cs
@@ -5,57 +5,41 @@ using TMPro;
 using UnityEngine.SceneManagement;
 
 public class ChangeMenu : MonoBehaviour {
-    [SerializeField] private List<GameObject> Panel = new List<GameObject>();
-    [SerializeField] private List<TextMeshProUGUI> TextButton;
+    [SerializeField] internal List<GameObject> Panel = new List<GameObject>();
     [SerializeField] private MainMenuManager mainMenuManager;
     [SerializeField] private List<Data> datas;
     void Start() {
-        Panel[0].SetActive(false); // Options
-        Panel[1].SetActive(false); // credit
+        Panel[0].SetActive(false);
+        Panel[1].SetActive(false);
     }
 
     public void DisplayOptions() {
-        for (int i = 0; i < Panel.Count; i++) {
-            Panel[i].SetActive(false);
-        }
-        //PanelAnimator[0].SetTrigger("TriggerOption");
-        Panel[0].SetActive(true);
-        Panel[0].transform.SetAsFirstSibling();
+        mainMenuManager.StartCoroutine(mainMenuManager.BlackscreenTransitionInMainMenu(0));
     }
 
     public void DisplayCredit() {
-        for (int i = 0; i < Panel.Count; i++) {
-            Panel[i].SetActive(false);
-        }
-        Panel[1].SetActive(true);
+        mainMenuManager.StartCoroutine(mainMenuManager.BlackscreenTransitionInMainMenu(1));
     }
 
     public void DisplayMainMenu() {
-        for (int i = 0; i < Panel.Count; i++) {
-            Panel[i].SetActive(false);
-            for (int j = 0; j < TextButton.Count; j++) {
-                TextButton[j].color = new Color(50, 50, 50, 0);
-
-            }
-        }
-        Panel[2].SetActive(true);
-        mainMenuManager.displayMainMenuTools();
+        mainMenuManager.StartCoroutine(mainMenuManager.BlackscreenTransitionInMainMenu(2));
     }
 
     public void LoadTutorial() {
-        SceneManager.LoadScene("Tutorial");
+        mainMenuManager.StartCoroutine(mainMenuManager.BlackscreenTransition("Tutorial"));
     }
 
     public void Quit() {
+        mainMenuManager.StartCoroutine(mainMenuManager.BlackscreenTransition("quit"));
         foreach (Data data in datas)
             data.ResetValues();
-        Application.Quit();
+        
     }
 
     public void LoadBakery() {
+        mainMenuManager.StartCoroutine(mainMenuManager.BlackscreenTransition("FirstBakery"));
         foreach (Data data in datas)
             data.ResetValues();
-        SceneManager.LoadScene("FirstBakery");
     }
 
 }
--- Assets/Scripts/UI/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> Screen = new List<GameObject>();
     [SerializeField] private List<GameObject> Button = new List<GameObject>();
-    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private ChangeMenu InternalMenu;
+    [SerializeField] private GameObject Blackscreen;
+    private Animator blackscreenAnimator;
 
-
+    private void Start()
+    {
+        blackscreenAnimator = Blackscreen.GetComponent<Animator>();
+        blackscreenAnimator.SetTrigger("Fade");
+    }
 
 
     public void displayMainMenuTools()
@@ -34,6 +40,36 @@ public class MainMenuManager : MonoBehaviour
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public IEnumerator BlackscreenTransition(string SceneToLoad)
+    {
+        blackscreenAnimator.SetTrigger("FadeReverse");
+        yield return new WaitForSeconds(0.45f);
+        print("After delay");
+        if(SceneToLoad != "quit")
+        {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+
+    public IEnumerator BlackscreenTransitionInMainMenu(int MenuToSwitch)
+    {
+        blackscreenAnimator.SetTrigger("FadeReverse");
+        yield return new WaitForSeconds(0.25f);
+        blackscreenAnimator.SetTrigger("Fade");
+        
+        for (int i = 0; i < InternalMenu.Panel.Count; i++)
+        {
+            InternalMenu.Panel[i].SetActive(false);
+        }
+
+        InternalMenu.Panel[MenuToSwitch].SetActive(true);
+        InternalMenu.Panel[MenuToSwitch].transform.SetAsFirstSibling();
     }
 
 }
