95ce432ca32de6d60429414c1edf137d2f419a52
Arnaud Benier Moine <arnaudbm26@gmail.com>
2023-01-31 11:43:20 +0100

    Localized String Static Implementation

---
+++ Assets/Localization/LocalizedStringComponent.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Tables;
+
+public class LocalizedStringComponent : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private LocalizedStringTable table;
+    [SerializeField] private string key;
+
+    void OnEnable() {
+        table.TableChanged += LoadStrings;
+    }
+
+    void OnDisable() {
+        table.TableChanged -= LoadStrings;
+    }
+
+    private void LoadStrings(StringTable stringTable) {
+        text.text = GetLocalizedString(stringTable, key);
+    }
+    static string GetLocalizedString(StringTable table, string entryName) {
+        // Get the table entry. The entry contains the localized string and Metadata
+        StringTableEntry entry = table.GetEntry(entryName);
+        return entry.GetLocalizedString(); // We can pass in optional arguments for Smart Format or String.Format here.
+    }
+}
