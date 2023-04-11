3416309ae0792128cda842033f2cd48a9a549e25
Piploo0 <78150909+Piploo0@users.noreply.github.com>
2023-04-11 16:39:52 +0200

    Push

--- Assets/Scripts/UI/Text/DayTimeUI.cs
+++ Assets/Scripts/UI/Text/DayTimeUI.cs
@@ -3,59 +3,74 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class DayTimeUI : MonoBehaviour {
+public class DayTimeUI : MonoBehaviour
+{
     [SerializeField] private Day day;
     private LocalizedStringComponent text;
     private int duration;
 
-    private void Start() {
+    private void Start()
+    {
         text = GetComponent<LocalizedStringComponent>();
         SetDay();
     }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         day.DayTimeChange += SetDay;
     }
 
-    private void OnDisable() {
+    private void OnDisable()
+    {
         day.DayTimeChange -= SetDay;
     }
 
-    private void SetDay() {
-        text.SetKey(GetDayTime());
-    }
-
-    //private IEnumerator TimeRemaining() {
-    //    duration = day.GetDayDuration();
-
-    //    int timeRemaining = duration - day.GetTimeElapsed();
-
-    //    text.SetText(GetDayTime() + " " + timeRemaining / 60 + ":"); // Display minutes
-    //    if (timeRemaining % 60 < 10)
-    //        text.text += "0" + timeRemaining % 60;
-    //    else
-    //        text.text += timeRemaining % 60;
-
-    //    yield return new WaitForSeconds(1);
-    //    day.AddTimeElpased(1);
-    //    if (day.GetTimeElapsed() > 0 && day.GetDayTime() != DayTime.Evening)
-    //        StartCoroutine(TimeRemaining());
-    //}
-
-    public string GetDayTime() {
-        string s = "";
-        switch (day.GetDayTime()) {
+    private void SetDay()
+    {
+        switch (day.GetDayTime())
+        {
             case DayTime.Day:
-                s = "Day";
+                text.SetKey("Day");
                 break;
             case DayTime.Evening:
-                s = "Evening";
-                break;
-            default:
+                text.SetKey("Evening");
                 break;
         }
-        return s;
     }
 
-    public Day GetDay() => day;
-}
+        //private IEnumerator TimeRemaining() {
+        //    duration = day.GetDayDuration();
+
+        //    int timeRemaining = duration - day.GetTimeElapsed();
+
+        //    text.SetText(GetDayTime() + " " + timeRemaining / 60 + ":"); // Display minutes
+        //    if (timeRemaining % 60 < 10)
+        //        text.text += "0" + timeRemaining % 60;
+        //    else
+        //        text.text += timeRemaining % 60;
+
+        //    yield return new WaitForSeconds(1);
+        //    day.AddTimeElpased(1);
+        //    if (day.GetTimeElapsed() > 0 && day.GetDayTime() != DayTime.Evening)
+        //        StartCoroutine(TimeRemaining());
+        //}
+
+        public string GetDayTime()
+        {
+            string s = "";
+            switch (day.GetDayTime())
+            {
+                case DayTime.Day:
+                    s = "Day";
+                    break;
+                case DayTime.Evening:
+                    s = "Evening";
+                    break;
+                default:
+                    break;
+            }
+            return s;
+        }
+
+        public Day GetDay() => day;
+    }
